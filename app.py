4935a84e54c2731375f1c6dd8f799a7f311135bd
Ananya0605 <ananyaverma1501@gmail.com>
2026-04-07 14:27:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, request, redirect
+
 app = Flask(__name__)
+
+posts = []
 
 @app.route('/')
 def home():
-    return "Hello from DevOps Project"
+    content = "<h1>My DevOps Blog</h1>"
+    content += "<form method='POST' action='/add'>"
+    content += "<input name='post' placeholder='Write something'>"
+    content += "<button>Add</button></form><hr>"
+
+    for p in posts:
+        content += f"<p>{p}</p>"
+
+    return content
+
+@app.route('/add', methods=['POST'])
+def add():
+    post = request.form['post']
+    posts.append(post)
+    return redirect('/')
 
 app.run(host='0.0.0.0', port=5000)
